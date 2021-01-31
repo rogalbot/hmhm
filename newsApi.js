224350f89f8a6ae95a26173e99bc4a87cab44356
Czesiek <czesiek@users.noreply.github.com>
2021-01-31 18:15:54 +0100

    Initial commit

---
+++ newsApi.js
@@ -0,0 +1,5 @@
+const apiKey = '5c279d36b6534f439b303cc3365503d9';
+const topHeadlinesUrl = 
+      'http://newsapi.org/v2/top-headlines?sources=techcrunch&apiKey=' + apiKey;
+
+export {apiKey, topHeadlinesUrl};
